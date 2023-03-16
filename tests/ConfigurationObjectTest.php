3cc887855abc557bb55c21259866e56e8ed849d6
Ludovic Muller <ludovicmuller1@gmail.com>
2023-03-16 11:03:46 +0100

    tests: add more tests to cover ConfigurationObject

--- tests/ConfigurationObjectTest.php
+++ tests/ConfigurationObjectTest.php
@@ -28,4 +28,25 @@ final class ConfigurationObjectTest extends TestCase {
       'fallback_url' => 'http://localhost/'
     ], $config->get());
   }
+
+  public function testGetValue(): void {
+    $config = new ConfigurationObject((object) [
+      'fallback_url' => 'http://localhost/',
+    ]);
+    $this->assertEquals('http://localhost/', $config->get('fallback_url'));
+  }
+
+  public function testGetNullValue(): void {
+    $config = new ConfigurationObject((object) [
+      'fallback_url' => null,
+    ]);
+    $this->assertEquals(null, $config->get('fallback_url'));
+  }
+
+  public function testGetNonExistingKey(): void {
+    $config = new ConfigurationObject((object) [
+      'fallback_url' => 'http://localhost/',
+    ]);
+    $this->assertEquals(null, $config->get('some-random-key'));
+  }
 }
